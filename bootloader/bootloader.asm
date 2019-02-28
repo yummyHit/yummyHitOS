f1b4ea194b14abf592ecb5d8fd53e19f16ed5551
yummyHit <crazyboyi@naver.com>
2019-03-01 03:29:09 +0900

    190228/finish! only writer encode..

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -25,9 +25,14 @@ START:
 	; 스택을 0x0000:0000~0x0000:FFFF 영역에 64KB 크기로 생성
 	mov ax, 0x0000		; 스택 세그먼트의 시작 어드레스(0x0000)를 세그먼트 레지스터 값으로 변환
 	mov ss, ax		; SS 세그먼트 레지스터에 설정
-	mov sp, 0xFFFF		; SP 레지스터의 어드레스를 0xFFFF로 설정
-	mov bp, 0xFFFF		; BP 레지스터의 어드레스를 0xFFFF로 설정
+	; 19.02.28 disk error
+;	mov sp, 0xFFFF		; SP 레지스터의 어드레스를 0xFFFF로 설정
+;	mov bp, 0xFFFF		; BP 레지스터의 어드레스를 0xFFFF로 설정
+	mov sp, 0xFFFE		; SP 레지스터의 어드레스를 0xFFFE로 설정
+	mov bp, 0xFFFE		; BP 레지스터의 어드레스를 0xFFFE로 설정
 
+	; 19.02.27 BOOTDRIVE 를 통해 드라이브 번호 읽는 부분 추가
+	mov byte [ BOOTDRIVE ], dl		; 부팅한 드라이브 번호를 메모리에 저장
 	mov si,	0		; SI 레지스터(문자열 원본 인덱스 레지스터)를 초기화
 	call MONITORCLEAR
 
@@ -41,11 +46,28 @@ START:
 ; 디스크를 리셋하는 코드의 시작
 RESETDISK:
 	; BIOS Reset Function 호출. 서비스 번호 0, 드라이브 번호(0 = Floppy)
-	mov ah, 0x00
-	mov dl, 0
+	; 19.02.27 BOOTDRIVE 와 함께 주석처리
+;	mov ah, 0x00
+;	mov dl, 0
+	call SKIPWITHHIT		; 19.02.28 disk error
+	mov ah, 0
+	mov dl, byte [ BOOTDRIVE ]
 	int 0x13
 	; 에러가 발생하면 에러 처리로 이동
 	jc HANDLEDISKERR
+
+	; 19.02.27 디스크 파리미터 읽는 부분 추가
+	call SKIPWITHLOAD		; 19.02.28 disk error
+	mov ah, 0x08					; BIOS 서비스 번호 8(Read Disk Parameters)
+	mov dl, byte [ BOOTDRIVE ]		; 읽을 드라비브 번호(USB Flash Drive) 설정
+	int 0x13						; 인터럽트 서비스 수행
+	jc HANDLEDISKERR				; 에러가 발생하면 에러 처리로 이동
+
+	mov byte [ LASTHEAD ], dh		; 헤드 정보 메모리에 저장
+	mov al, cl						; 섹터와 트랙 정보를 AL 레지스터에 저장
+	and al, 0x3F					; 섹터 정보(하위 6비트) 추출해 AL 레지스터에 저장
+	mov byte [ LASTSECTOR ], al		; 섹터 정보 메모리에 저장
+	mov byte [ LASTTRACK ], ch		; 트랙 정보 중 하위 8비트 메모리에 저장
 
 	; 디스크에서 섹터 읽음. 디스크의 내용을 메모리로 복사할 어드레스(ES:BX)를 0x10000으로 설정
 	mov si, 0x1000			; OS 이미지를 복사할 어드레스(0x10000)를 세그먼트 레지스터 값으로 변환
@@ -64,12 +86,15 @@ RESETDISK:
 	sub di, 0x1			; 복사할 섹터 수를 1 감소
 
 	; BIOS Read Function 호출
+	call SKIPWITHGUI		; 19.02.28 disk error
 	mov ah, 0x02			; BIOS 서비스 번호 2(Read Sector)
 	mov al, 0x1			; 읽을 섹터 수는 1
 	mov ch, byte [ TRACKNUM ]	; 읽을 트랙 번호 설정
 	mov cl, byte [ SECTORNUM ]	; 읽을 섹터 번호 설정
 	mov dh, byte [ HEADNUM ]	; 읽을 헤드 번호 설정
-	mov dl, 0x00			; 읽을 드라이브 번호(0=Floppy) 설정
+	; 19.02.27 Only floppy  에서 드라이브 번호 가져와 설정하는 것으로 변경
+;	mov dl, 0x00			; 읽을 드라이브 번호(0=Floppy) 설정
+	mov dl, byte [ BOOTDRIVE ]	; 읽을 드라이브 번호 설정
 	int 0x13			; 인터럽트 서비스 수행
 	jc HANDLEDISKERR		; 에러가 발생했다면 HANDLEDISKERR로 이동
 
@@ -82,18 +107,31 @@ RESETDISK:
 	mov al, byte [ SECTORNUM ]	; 섹터 번호를 AL 레지스터에 설정
 	add al, 0x01			; 섹터 번호를 1 증가
 	mov byte [ SECTORNUM ], al	; 증가시킨 섹터 번호를 SECTORNUM에 다시 설정
-	cmp al, 19			; 증가시킨 섹터 번호를 19와 비교
-	jl .READDATA			; 섹터 번호가 19 미만이라면 리드로 이동
+	; 19.02.27 Only floppy 에서 읽은 섹터에 따라 수행토록 변경
+;	cmp al, 19			; 증가시킨 섹터 번호를 19와 비교
+;	jl .READDATA			; 섹터 번호가 19 미만이라면 리드로 이동
+	cmp al, byte [ LASTSECTOR ]		; 증가시킨 섹터 번호를 마지막 섹터 번호와 비교
+	jbe .READDATA					; 섹터 번호가 마지막 섹터 이하면 리드로 이동
 
 	; 마지막 섹터까지 읽었으면(섹터 번호가 19이면) 헤드를 토글(0->1, 1->0) 하고, 섹터 번호 1로 설정
-	xor byte [ HEADNUM ], 0x01	; 헤드 번호를 0x01과 XOR하여 토글
+	; 19.02.27 헤드가 2개이던 플로피와 다르게 얼마나 있을지 모르므로 add 형식으로 변경
+;	xor byte [ HEADNUM ], 0x01	; 헤드 번호를 0x01과 XOR하여 토글
+	add byte [ HEADNUM ], 0x01	; 헤드 번호 1 증가
 	mov byte [ SECTORNUM ], 0x01	; 섹터 번호를 다시 1로 설정
 
 	; 만약 헤드가 1->0 으로 바뀌었으면 양쪽 헤드를 모두 읽은 것이므로 아래로 이동하여 트랙 번호 1 증가
-	cmp byte [ HEADNUM ], 0x00	; 헤드 번호를 0x00과 비교
-	jne .READDATA			; 헤드 번호가 0이 아니면 리드로 이동
+	; 19.02.27 섹터->헤드->트랙 증가하는 구문 수정
+;	cmp byte [ HEADNUM ], 0x00	; 헤드 번호를 0x00과 비교
+;	jne .READDATA			; 헤드 번호가 0이 아니면 리드로 이동
+	mov al, byte [ LASTHEAD ]		; 마지막 헤드 번호를 AL 레지스터에 설정
+	cmp byte [ HEADNUM ], al		; 헤드 번호를 마지막 헤드 번호와 비교
+	jg .ADDTRACK					; 마지막 헤드 번호보다 크면 트랙 번호 1 증가
+	jmp .READDATA
 
+.ADDTRACK
 	; 트랙을 1 증가시킨 후 다시 섹터 읽기로 이동
+	; 19.02.27 섹터->헤드->트랙 증가하는 구문 수정
+	mov byte [ HEADNUM ], 0x00	; 헤드 번호 0 으로 설정
 	add byte [ TRACKNUM ], 0x01	; 트랙 번호 1 증가
 	jmp .READDATA
 
@@ -148,11 +186,42 @@ RESETDISK:
 HANDLEDISKERR:			; 에러 처리 코드
 	push ERRMSG		; 에러 문자열의 어드레스를 스택에 삽입
 	push 0x1C		; 빨간색
-	push 1			; 화면 Y 좌표(1)를 스택에 삽입
-	push 57			; 화면 X 좌표(57)를 스택에 삽입
+;	push 1			; 화면 Y 좌표(1)를 스택에 삽입
+;	push 57			; 화면 X 좌표(57)를 스택에 삽입
+	push 10			; 화면 Y 좌표(10)를 스택에 삽입
+	push 33			; 화면 X 좌표(35)를 스택에 삽입
 	call PRINTMSG		; PRINTMSG 함수 호출
 	add sp, 8
 	jmp $			; 현재 위치에서 무한 루프 수행
+
+; disk error 없애기 위한 3가지 함수.
+; 1. MSG 문자가 있으나 없으나 disk error
+; 2. 하나의 함수를 3번 call 하면 그 또한 disk error
+; 3. 아래와 같이 3개 함수로 나누면 ok
+; 4. boot option 에서 usb 인식을 하여 부트로더를 찾으면 ok
+SKIPWITHHIT:
+	push HITMSG	
+	push 0x1F
+	push 7			; 화면 Y 좌표(9)를 스택에 삽입
+	push 35			; 화면 X 좌표(35)를 스택에 삽입
+	call PRINTMSG		; PRINTMSG 함수 호출
+	add sp, 8
+
+SKIPWITHLOAD:
+	push LOADINGMSG	
+	push 0x1F
+	push 8			; 화면 Y 좌표(9)를 스택에 삽입
+	push 35			; 화면 X 좌표(35)를 스택에 삽입
+	call PRINTMSG		; PRINTMSG 함수 호출
+	add sp, 8
+
+SKIPWITHGUI:
+	push GRAPHICERRMSG
+	push 0x1F
+	push 9			; 화면 Y 좌표(9)를 스택에 삽입
+	push 35			; 화면 X 좌표(35)를 스택에 삽입
+	call PRINTMSG		; PRINTMSG 함수 호출
+	add sp, 8
 
 ; 메시지를 출력하는 함수
 ; PARAM : x 좌표, y 좌표, 문자열
@@ -244,15 +313,26 @@ MONITORCLEAR:
 	pop bp
 	ret
 
-ERRMSG:	db '[  ERR  ]', 0
-LOADINGMSG:	db 'OS image Loading .................................', 0
-HITMSG:	db '[  Hit  ]', 0
-GRAPHICERRMSG : db 'Change Graphic Mode ..............................', 0
+; 19.02.27 파티션 테이블 read 를 위해 메세지 부분 최소화
+;ERRMSG:	db '[  ERR  ]', 0
+;LOADINGMSG:	db 'OS image Loading .................................', 0
+;HITMSG:	db '[  Hit  ]', 0
+;GRAPHICERRMSG : db 'Change Graphic Mode ..............................', 0
+ERRMSG:	db 'Disk Read Error', 0
+LOADINGMSG:	db '', 0
+HITMSG:	db '', 0
+GRAPHICERRMSG : db '', 0
 
 ; 디스크 읽기에 관련된 변수들
-SECTORNUM:	db 0x02    ; OS 이미지가 시작하는 섹터 번호를 저장하는 영역
-HEADNUM:	db 0x00    ; OS 이미지가 시작하는 헤드 번호를 저장하는 영역
-TRACKNUM:	db 0x00    ; OS 이미지가 시작하는 트랙 번호를 저장하는 영역
+SECTORNUM:	db 0x02		; OS 이미지가 시작하는 섹터 번호를 저장하는 영역
+HEADNUM:	db 0x00		; OS 이미지가 시작하는 헤드 번호를 저장하는 영역
+TRACKNUM:	db 0x00		; OS 이미지가 시작하는 트랙 번호를 저장하는 영역
+
+; 디스크 파라미터 관련 변수들
+BOOTDRIVE:	db 0x00		; 부팅 드라이브 번호 저장 영역
+LASTSECTOR:	db 0x00		; 드라미브 마지막 섹터 번호 -1 저장 영역
+LASTHEAD:	db 0x00		; 드라이브 마지막 헤드 번호 저장 영역
+LASTTRACK:	db 0x00		; 드라이브 마지막 트랙 번호 저장 영역
 
 times 510 - ( $ - $$ )	db	0x00	; $ : 현재 라인의 어드레스
 					; $$ : 현재 섹션의 시작 어드레스
